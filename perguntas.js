147050b3526fad69952235139cc450b6f7eaddfd
Feefesn <fsn1802@gmail.com>
2024-10-22 18:53:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Compras',
+    'Salto da YSL ou Salto normal?',
+    'Salto YSL '
+)
+
+criaCartao(
+    'Maquiagem',
+    'Qual a unica maquaigem que eu preciso?',
+    'Blush'
+)
+
+criaCartao(
+    'Compras',
+    'O que é bobs?',
+    'Bobs é uma forma de modelar o cabelo'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Oque é deliniado ?',
+    'É uma forma de realçar os olhos'
+)
